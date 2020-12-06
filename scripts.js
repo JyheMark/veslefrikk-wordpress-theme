f773b5185eaffa1bc4de52fe793d73202a63e60e
jyhemark <randomdude2610@hotmail.com>
2020-12-06 01:06:04 +0100

    mobile view

--- scripts.js
+++ scripts.js
@@ -13,11 +13,24 @@ window.onscroll = function resizeNavBar() {
     let navBar = document.getElementById('nav-container');
 
     if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
-        navBar.style.padding = '5px 70px';
+        navBar.style.paddingTop = '5px';
+        navBar.style.paddingBottom = '5px';
         updateHeaderTopMargin(Math.floor(0));
     }
     else {
-        navBar.style.padding = '25px 70px';
+        navBar.style.paddingTop = '25px';
+        navBar.style.paddingBottom = '25px';
         updateHeaderTopMargin(Math.floor(navBar.scrollHeight-1));
+    }
+}
+
+function toggleMobileMenu() {
+    let navMenu = document.getElementById('mobile-menu');
+
+    if (navMenu.className === 'mobile-menu'){
+        navMenu.className = 'mobile-menu expanded';
+    }
+    else {
+        navMenu.className = 'mobile-menu';
     }
 }
